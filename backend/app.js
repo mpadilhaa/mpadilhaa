6abee83546c384c1e059fec6739e7bb34d3ef96f
matheus padilha <mpadilhaac@outlook.com>
2022-09-08 20:32:12 -0300

    ReactInstaClone

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,4 @@
-require("dotenv").config
+require("dotenv").config()
 
 const express = require("express");
 const path = require("path")
@@ -9,6 +9,11 @@ const port= process.env.PORT
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+//Routes
+const router = require("./routes/Router.js");
+
+app.use(router)
 
 app.listen(port, () =>{
     console.log(`o servidor esta rodando na porta ${port}`)
